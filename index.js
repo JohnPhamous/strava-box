35a07ec5a31ea08bf1ff2ed873eba6e25ae2787e
John Pham <johnphammail@gmail.com>
2019-06-22 15:38:42 -0700

    Add Strava API

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 require("dotenv").config();
-const { WakaTimeClient, RANGE } = require("wakatime-client");
 const Octokit = require("@octokit/rest");
+const fetch = require("node-fetch");
 
 const {
   GIST_ID: gistId,
   GITHUB_TOKEN: githubToken,
-  WAKATIME_API_KEY: wakatimeApiKey
+  STRAVA_ATHLETE_ID: stravaAtheleteId,
+  STRAVA_ACCESS_TOKEN: stravaAccessToken,
+  UNITS: units
 } = process.env;
-
-const wakatime = new WakaTimeClient(wakatimeApiKey);
 
 const octokit = new Octokit({
   auth: `token ${githubToken}`
 });
 
 async function main() {
-  const stats = await wakatime.getMyStats({ range: RANGE.LAST_7_DAYS });
+  const stats = await getStravaStats();
   await updateGist(stats);
 }
 
-async function updateGist(stats) {
+/**
+ * Fetches your data from the Strava API
+ * The distance returned by the API is in meters
+ */
+async function getStravaStats() {
+  const API_BASE = "https://www.strava.com/api/v3/athletes/";
+  const API = `${API_BASE}${stravaAtheleteId}/stats?access_token=${stravaAccessToken}`;
+
+  const data = await fetch(API);
+  const json = await data.json();
+
+  return json;
+}
+
+async function updateGist(data) {
   let gist;
   try {
     gist = await octokit.gists.get({ gist_id: gistId });
@@ -27,20 +41,59 @@ async function updateGist(stats) {
     console.error(`Unable to get gist\n${error}`);
   }
 
-  const lines = [];
-  for (let i = 0; i < 5; i++) {
-    const data = stats.data.languages[i];
-    const { name, percent, text: time } = data;
+  // Used to index the API response
+  const keyMappings = {
+    Running: {
+      key: "ytd_run_totals"
+    },
+    Swimming: {
+      key: "ytd_swim_totals"
+    },
+    Biking: {
+      key: "ytd_ride_totals"
+    }
+  };
 
-    const line = [
-      name.padEnd(11),
-      time.padEnd(14),
-      generateBarChart(percent, 12),
-      String(percent.toFixed(1)).padStart(5) + "%"
-    ];
+  let totalDistance = 0;
 
-    lines.push(line.join(" "));
-  }
+  // Store the activity name and distance
+  let activities = Object.keys(keyMappings).map(activityType => {
+    const { key } = keyMappings[activityType];
+    const { distance } = data[key];
+
+    totalDistance += distance;
+
+    return {
+      name: activityType,
+      distance
+    };
+  });
+
+  // Calculate the percentages and bar charts for the 3 activities
+  activities = activities.map(activity => {
+    const percent = (activity["distance"] / totalDistance) * 100;
+    return {
+      ...activity,
+      percent: percent.toFixed(1),
+      barChart: generateBarChart(percent, 12)
+    };
+  });
+
+  // Append and/or convert the distance units
+  activities = activities.map(activity => {
+    return {
+      ...activity,
+      distance: formatDistance(activity["distance"])
+    };
+  });
+
+  // Format the data to be displayed in the Gist
+  const lines = activities.map(activity => {
+    const { name, distance, percent, barChart } = activity;
+    return `${name.padEnd(10)} ${distance.padEnd(
+      10
+    )} ${barChart} ${percent.padStart(5)}%`;
+  });
 
   try {
     // Get original filename to update that same file
@@ -49,7 +102,7 @@ async function updateGist(stats) {
       gist_id: gistId,
       files: {
         [filename]: {
-          filename: `Weekly Programming Metrics`,
+          filename: `YTD Strava Metrics`,
           content: lines.join("\n")
         }
       }
@@ -64,6 +117,23 @@ function generateBarChart(percent, size) {
   const full = "█";
   const barsFull = Math.round(size * (percent / 100));
   return full.repeat(barsFull).padEnd(size, empty);
+}
+
+function formatDistance(distance) {
+  const trimmedDistance = parseFloat(distance).toFixed(2);
+  switch (units) {
+    case "meters":
+      return `${trimmedDistance} m`;
+    case "miles":
+      return `${metersToMiles(distance)} mi`;
+    default:
+      return `${trimmedDistance} m`;
+  }
+}
+
+function metersToMiles(meters) {
+  const CONVERSION_CONSTANT = 0.000621371192;
+  return (meters * CONVERSION_CONSTANT).toFixed(2);
 }
 
 (async () => {
