d2c6e7d005036ccbc53dbfb92b5fbc1495906517
John Pham <johnphammail@gmail.com>
2019-06-22 14:36:53 -0700

    Project init

---
+++ README.md
@@ -0,0 +1,29 @@
+<p align="center">
+  <img width="400" src="https://user-images.githubusercontent.com/4658208/54497463-4be25680-48d1-11e9-9f84-1ca605ae100f.png">
+  <h3 align="center">waka-box</h3>
+  <p align="center">Update a pinned gist to contain your weekly WakaTime stats</p>
+</p>
+
+---
+
+## Setup
+
+### Prep work
+
+1. Create a new public GitHub Gist (https://gist.github.com/)
+1. Create a token with the `gist` scope and copy it. (https://github.com/settings/tokens/new)
+1. Create a WakaTime account (https://wakatime.com/signup)
+1. In your account settings, copy the existing WakaTime API Key (https://wakatime.com/settings/account)
+
+### Project setup
+
+1. Fork this repo
+1. Log into CircleCI with your GitHub (https://circleci.com/vcs-authorize/)
+1. Click on "Add Projects" on the sidebar
+1. Set up a project with the newly created fork
+1. Go to Project Settings > Environment Variables
+1. Add the following environment variables:
+
+- **GIST_ID:** The ID portion from your gist url `https://gist.github.com/matchai/`**`6d5f84419863089a167387da62dd7081`**.
+- **GITHUB_TOKEN:** The GitHub token generated above.
+- **WAKATIME_API_KEY:** The API key for your WakaTime account.
---
+++ index.js
@@ -0,0 +1,71 @@
+require("dotenv").config();
+const { WakaTimeClient, RANGE } = require("wakatime-client");
+const Octokit = require("@octokit/rest");
+
+const {
+  GIST_ID: gistId,
+  GITHUB_TOKEN: githubToken,
+  WAKATIME_API_KEY: wakatimeApiKey
+} = process.env;
+
+const wakatime = new WakaTimeClient(wakatimeApiKey);
+
+const octokit = new Octokit({
+  auth: `token ${githubToken}`
+});
+
+async function main() {
+  const stats = await wakatime.getMyStats({ range: RANGE.LAST_7_DAYS });
+  await updateGist(stats);
+}
+
+async function updateGist(stats) {
+  let gist;
+  try {
+    gist = await octokit.gists.get({ gist_id: gistId });
+  } catch (error) {
+    console.error(`Unable to get gist\n${error}`);
+  }
+
+  const lines = [];
+  for (let i = 0; i < 5; i++) {
+    const data = stats.data.languages[i];
+    const { name, percent, text: time } = data;
+
+    const line = [
+      name.padEnd(11),
+      time.padEnd(14),
+      generateBarChart(percent, 12),
+      String(percent.toFixed(1)).padStart(5) + "%"
+    ];
+
+    lines.push(line.join(" "));
+  }
+
+  try {
+    // Get original filename to update that same file
+    const filename = Object.keys(gist.data.files)[0];
+    await octokit.gists.update({
+      gist_id: gistId,
+      files: {
+        [filename]: {
+          filename: `Weekly Programming Metrics`,
+          content: lines.join("\n")
+        }
+      }
+    });
+  } catch (error) {
+    console.error(`Unable to update gist\n${error}`);
+  }
+}
+
+function generateBarChart(percent, size) {
+  const empty = "░";
+  const full = "█";
+  const barsFull = Math.round(size * (percent / 100));
+  return full.repeat(barsFull).padEnd(size, empty);
+}
+
+(async () => {
+  await main();
+})();
